45bda1acb899d37f9e02323afc545ff4471b556a
Andrei Campean <andreicampean@gmail.com>
2017-04-08 21:53:11 +0300

    stilizare tabel firme

--- insert_firme.php
+++ insert_firme.php
@@ -5,8 +5,11 @@
     $sql_aratafirma = "SELECT * FROM lista_firme";
     $result_aratafirma = mysqli_query($conn, $sql_aratafirma);
 
-    echo "<table class='table_firme_delegat'>
-            <caption>Firme</caption>
+    echo "<h2 class='text-center'>Lista firmelor</h2> <br> <br>
+          <div class='row'>
+          <div class='container-fluid'>
+          <div class='table-responsive'>
+          <table class='table'>
             <tr>
               <th>id</th>
               <th>id_firma</th>
@@ -30,20 +33,31 @@
               echo "<td>". $row_aratafirma['judet']. "</td></tr>";
               echo "<br>";
             }
-            echo "<form class='' action='includes/insert_firme.inc.php' method='post'>
-                    <tr>
-                      <td></td>
-                      <td><input type='text' name='id_firma' placeholder='id firma'> </td>
-                      <td><input type='text' name='firma' placeholder='nume firma'> </td>
-                      <td><input type='text' name='cif' placeholder='CIF'> </td>
-                      <td><input type='text' name='nr_inm_reg_com' placeholder='nr_inm_reg_com'> </td>
-                      <td><input type='text' name='localitate' placeholder='localitatea'> </td>
-                      <td><input type='text' name='strada' placeholder='strada'> </td>
-                      <td><input type='text' name='nr' placeholder='nr'> </td>
-                      <td><input type='text' name='judet' placeholder='judet'> </td>
-                      <td><input type='submit' value='Insert'> </td>
-                    </tr></form>";
-      echo "</table>";
+      echo "</table></div></div></div></div>";
+
+      $url = "http://".$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI'];
+      if(strpos($url, 'error=empty') != false){
+        echo "you left one or more fileds empty";
+      }
       ?>
+
+      <form class="" action="includes/insert_firme.inc.php" method="post">
+        <tr>
+          <td></td>
+          <td><input type="text" name="id_firma" placeholder="id firma"> </td>
+          <td><input type="text" name="firma" placeholder="nume firma"> </td>
+          <td><input type="text" name="cif" placeholder="CIF"> </td>
+          <td><input type="text" name="nr_inm_reg_com" placeholder="nr_inm_reg_com"> </td>
+          <td><input type="text" name="localitate" placeholder="localitatea"> </td>
+          <td><input type="text" name="strada" placeholder="strada"> </td>
+          <td><input type="text" name="nr" placeholder="nr"> </td>
+          <td><input type="text" name="judet" placeholder="judet"> </td>
+          <td><input type="submit" value="Insert"> </td>
+        </tr>
+      </form>
+    </section>
+    <?php
+      include 'footer.php';
+     ?>
   </body>
 </html>
